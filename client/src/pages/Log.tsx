c5f4c130d83b983e1e2024bd846ff0381dd66505
Sorrawit Amdaeng <sorrawit.amdaeng@gmail.com>
2026-05-20 13:28:08 +0700

    style: adjust loading and error states in Log component for better layout and user experience

--- client/src/pages/Log.tsx
+++ client/src/pages/Log.tsx
@@ -26,7 +26,7 @@ function Log() {
   return (
     <MainWithNavbar className="gap-4 items-center">
       <div className="flex flex-col gap-2 w-full md:flex-row md:justify-between ">
-        <h3 className="style-headline-3">Logs</h3>
+        <h3 className="style-headline-3">Log</h3>
         <div className="flex gap-2">
           <div className="flex gap-2 not-md:flex-1">
             <select
@@ -61,29 +61,25 @@ function Log() {
         </div>
       </div>
       {isLoading ? (
-        <div className="py-20">
-          <span className="loading loading-spinner loading-xl text-accent" />
+        <div className="flex h-[507px]">
+          <span className="loading loading-spinner loading-xl justify-center items-center text-accent" />
         </div>
       ) : logsError ? (
-        <div className="flex flex-col justify-center items-center gap-2 h-full py-16">
+        <div className="flex flex-col justify-center items-center gap-2 h-[507px]">
           <CircleX className="size-8" />
           <span className="style-body-1">{logsError}</span>
         </div>
       ) : logs.length ? (
-        <>
-          <div className="w-full overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
-            <ParkingLogTable page={page} limit={limit} logs={logs} />
-          </div>
-          <span className="style-body-3 text-info">
-            Total logs: {totalLogs}
-          </span>
-        </>
+        <div className="w-full h-[507px] overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
+          <ParkingLogTable page={page} limit={limit} logs={logs} />
+        </div>
       ) : (
-        <div className="flex flex-col justify-center items-center gap-2 py-16">
+        <div className="flex flex-col justify-center items-center gap-2 py-16 h-[507px]">
           <SearchX className="size-8" />
           <span className="style-body-1">No logs found.</span>
         </div>
       )}
+      <span className="style-body-3 text-info">Total logs: {totalLogs}</span>
       <ParkingLogPagination
         page={page}
         totalPages={totalPages}
