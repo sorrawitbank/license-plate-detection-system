611fb01262a1b472d02932dfd22c34d1eb04aac0
Sorrawit Amdaeng <sorrawit.amdaeng@gmail.com>
2026-05-22 16:04:24 +0700

    feat: update Sidebar component to use dynamic routing for active links based on current location

--- client/src/layout/Sidebar.tsx
+++ client/src/layout/Sidebar.tsx
@@ -1,11 +1,23 @@
 import { LayoutDashboard, Logs, Radar } from "lucide-react";
-import { Link } from "react-router-dom";
+import { Link, useLocation, type To } from "react-router-dom";
 
 interface Props {
   children?: React.ReactNode;
 }
 
+function sidebarTo(
+  pathname: string,
+  location: ReturnType<typeof useLocation>
+): To {
+  if (location.pathname === pathname) {
+    return { pathname, search: location.search };
+  }
+  return pathname;
+}
+
 function Sidebar(props: Props) {
+  const location = useLocation();
+
   return (
     <aside className="drawer lg:drawer-open">
       <input id="nav-drawer" type="checkbox" className="drawer-toggle" />
@@ -18,14 +30,17 @@ function Sidebar(props: Props) {
         />
         <ul className="menu gap-2 bg-base-200 min-h-full w-60 p-4">
           <li>
-            <Link to="/" className="btn btn-ghost btn-xl justify-start">
+            <Link
+              to={sidebarTo("/", location)}
+              className="btn btn-ghost btn-xl justify-start"
+            >
               <LayoutDashboard />
               Dashboard
             </Link>
           </li>
           <li>
             <Link
-              to="/detection"
+              to={sidebarTo("/detection", location)}
               className="btn btn-ghost btn-xl justify-start"
             >
               <Radar />
@@ -33,7 +48,10 @@ function Sidebar(props: Props) {
             </Link>
           </li>
           <li>
-            <Link to="/log" className="btn btn-ghost btn-xl justify-start">
+            <Link
+              to={sidebarTo("/log", location)}
+              className="btn btn-ghost btn-xl justify-start"
+            >
               <Logs />
               Log
             </Link>
